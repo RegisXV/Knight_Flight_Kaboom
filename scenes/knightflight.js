9b205b30c7fe64345e137291f1110b15bf8b673b
RegisXV <Kyle.mercadov@gmail.com>
2024-03-31 12:08:02 -0400

    Anims are now in the game. They are not yet used,

--- scenes/knightflight.js
+++ scenes/knightflight.js
@@ -1,9 +1,32 @@
 import k from '../kaboom.js'
 
-k.loadSprite('knight',"../assets/knight.png")
+
 // k.loadSprite('platforms',"../assets/Starter Tiles Platformer/BasicGreenGrid.png")
 // k.loadTiledMap('ground',"../assets/Starter Tiles Platformer/Ground.json")
 k.loadSprite('background',"../assets/oak_woods_v1.0/oak_woods_v1.0/background/background_layer_1.png")
+k.loadSpriteAtlas("../assets/knight.png", {
+    sliceX: 6,
+    sliceY: 1,
+    anims: {
+        idle: {
+            from: 11,
+            to: 19,
+        },
+        frun: {
+            from: 0,
+            to: 9,
+        },
+        jump: {
+            from: 20,
+            to: 22,
+        },
+        rrun: {
+            from: 23,
+            to: 32,
+        },
+    },
+})
+
 
 export default function Game () {{
     setGravity(2400)
@@ -13,13 +36,16 @@ k.add([
 
 ])
 const player = k.add([
-    k.rect(32, 32),
-    k.pos(k.width() * 0.5, 0),
+    k.sprite('knight'),
+    k.pos(80, 80),
     k.area(),
-    k.body()
+    k.body(),
 ])
+
+player.play('idle')
 k.onKeyPress('space', () => {
     player.jump()
+    player.play('jump')
 })
 console.log(player)
 }
