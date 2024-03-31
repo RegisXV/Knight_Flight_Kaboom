003a1d98522f1fa035e36102079bd1da66d3f56b
RegisXV <Kyle.mercadov@gmail.com>
2024-03-31 18:47:21 -0400

    Movement for knight

--- main.js
+++ main.js
@@ -5,6 +5,43 @@ import Game from "./scenes/knightflight.js";
 
 k.scene('hello_world',HelloWorld)
 k.scene('game', Game)
+
+
+k.loadSprite('background',"../assets/oak_woods_v1.0/oak_woods_v1.0/background/background_layer_1.png")
+k.loadSprite("knight","../assets/knightspritesheet.png", { 
+        sliceX: 11,
+        sliceY: 3,
+        width: 120,
+        height: 80,
+        anims: {
+            
+            "frun": {
+                from: 0,
+                to: 9,
+                speed: 10,
+                loop: true,
+            },    
+                "idle": {
+                from: 10,
+                to: 19,
+                speed: 5,
+                loop: true,
+            },
+            
+            "jump": {
+                from: 20,
+                to: 22,
+            },
+            "rrun": {
+                from: 32,
+                to: 23,
+                speed: 10,
+                loop: true,
+            },
+        },
+    })
+
+
 console.log('hello check 1')
 
 k.scene('main', () => {
--- scenes/knightflight.js
+++ scenes/knightflight.js
@@ -3,49 +3,68 @@ import k from '../kaboom.js'
 
 // k.loadSprite('platforms',"../assets/Starter Tiles Platformer/BasicGreenGrid.png")
 // k.loadTiledMap('ground',"../assets/Starter Tiles Platformer/Ground.json")
-k.loadSprite('background',"../assets/oak_woods_v1.0/oak_woods_v1.0/background/background_layer_1.png")
-k.loadSpriteAtlas("../assets/knight.png", {
-    sliceX: 6,
-    sliceY: 1,
-    anims: {
-        idle: {
-            from: 11,
-            to: 19,
-        },
-        frun: {
-            from: 0,
-            to: 9,
-        },
-        jump: {
-            from: 20,
-            to: 22,
-        },
-        rrun: {
-            from: 23,
-            to: 32,
-        },
-    },
-})
 
 
 export default function Game () {{
+    
     setGravity(2400)
 k.add([
     k.sprite('background'),
     k.scale(k.width() / 320, k.height()/240),
 
 ])
+k.add([
+    k.rect(k.width(), 50),
+    outline(10),
+    k.area(),
+    k.color(1,1,1),
+    pos(0, k.height() - 50),
+    body({isStatic: true}),
+])
 const player = k.add([
-    k.sprite('knight'),
+    k.sprite("knight", { anim: "idle" }),
     k.pos(80, 80),
     k.area(),
     k.body(),
 ])
+k.onKeyDown("right",()=>{
+    player.move(200,0)
+    if(player.isGrounded() && player.curAnim() !== "frun"){
+        player.play("frun")
+    }
+})
+k.onKeyDown("left",()=>{
+    player.move(-200,0)
+    if(player.isGrounded() && player.curAnim() !== "rrun"){
+        player.play("rrun")
+    }
+})
+;["left", "right"].forEach((key) => {
+	k.onKeyRelease(key, () => {
+		if (player.isGrounded() && !isKeyDown("left") && !isKeyDown("right")) {
+			player.play("idle")
+        }
+    })
+})
+player.onGround(() => {
+    if(!isKeyDown('left') && !isKeyDown('right')){
+        player.play("idle")
+    }
+    else if(isKeyDown('right')){
+        player.play("frun")
+    
+    }
+    else if(isKeyDown('left')){
+        player.play("rrun")
+    }
+})
 
-player.play('idle')
+
 k.onKeyPress('space', () => {
+    if(player.isGrounded()){
     player.jump()
-    player.play('jump')
+    player.play("jump")
+    }
 })
 console.log(player)
 }
